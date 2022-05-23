88b0b4a7607ce3f432be3ce8ea88285cc1508db0
Dominykas-Zernys <zernysdominykas@gmail.com>
2022-05-23 19:30:01 +0300

    digital ocean image test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,21 @@ import Wrapper from '../components/Wrapper/Wrapper';
 const Home = () => {
   return (
     <Main>
-      <Wrapper>Home</Wrapper>
+      <Wrapper>
+        <img src='img/img1.jpg' alt='' />
+        <img src='img/img2.jpg' alt='' />
+        <img src='img/img3.jpg' alt='' />
+        <img src='img/img4.avif' alt='' />
+        <img src='img/img5.jpg' alt='' />
+        <img src='img/img6.jpg' alt='' />
+        <img src='img/img7.webp' alt='' />
+        <img src='img/img10.png' alt='' />
+        <img src='img/img11.png' alt='' />
+        <img src='img/Advertise yourself-logos_black.png' alt='' />
+        <img src='img/Advertise yourself-logos_white.png' alt='' />
+        <img src='img/Advertise yourself-logos.jpeg' alt='' />
+        <img src='img/logo.png' alt='' />
+      </Wrapper>
     </Main>
   );
 };
